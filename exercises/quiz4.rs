cdc1978bfedcff5d0520b902adfa68ae8892ef71
Pablo Fco. Perez Hidalgo <contact@pablofranciscoperez.info>
2021-08-14 16:41:36 +0200

    Completed: macros

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,16 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+use std::fmt;
+
+#[macro_use]
+mod quiz4 {
+    macro_rules! my_macro {
+        ($x:expr) => {
+            format!("Hello {}", $x)
+        };
+    }
+}
 
 #[cfg(test)]
 mod tests {
